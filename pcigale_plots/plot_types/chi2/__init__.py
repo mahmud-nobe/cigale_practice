2527907d52d02c55546c38cfb79dc0c8e031749a
Md Mahmudunnobe <mahmud.nobe@wayne.edu>
2024-02-11 00:18:06 -0500

    check condition to avoid division by 0

--- pcigale_plots/plot_types/chi2/__init__.py
+++ pcigale_plots/plot_types/chi2/__init__.py
@@ -76,9 +76,8 @@ class Chi2(Plotter):
             chi2 = Table.read(fchi2)
             data_df = pd.DataFrame([ values[var_name], chi2["chi2"]], index = [var_name, 'chi2']).T
             if (min(data_df.chi2) == np.nan or min(data_df.chi2) == 0):
-                return(f'$chi^2_{min}$ is {data_df.chi2}')
-            else:
-                data_df['reduced_chi2'] = data_df.chi2 / min(data_df.chi2)
+                return(f'$chi^2_{min}$ is {min(data_df.chi2)}')
+            data_df['reduced_chi2'] = data_df.chi2 / min(data_df.chi2)
             filtered_df = data_df[data_df.reduced_chi2 <= 2]
             ax.scatter(filtered_df[var_name], filtered_df["chi2"], color="k", s=0.1)
         ax.set_xlabel(var_name)
